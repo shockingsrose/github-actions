e792332e337bf64c568326137c3204986dbdcfab
storm <847073376@qq.com>
2022-05-17 10:22:51 +0800

    test output

---
+++ scripts/tools.js
@@ -0,0 +1,17 @@
+const { promises: fs } = require('fs');
+const path = require('path');
+const pkg = require('../package.json')
+
+const changeLogPath = path.join(__dirname, '../CHANGELOG.md');
+
+const run = async () => {
+  const changeLogData = await fs.readFile(changeLogPath, { encoding: 'utf-8' })
+  const version = pkg.version;
+
+  const [, changeLog] = /(###\s+[\w.]+\n[\s\S]+)###/.exec(changeLogData);
+
+  console.log(changeLog);
+
+}
+
+run();
