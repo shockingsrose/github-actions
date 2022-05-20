973140c853d944a264151e88c7b4ccb7ee4eac5b
storm <847073376@qq.com>
2022-05-20 11:05:01 +0800

    新建publish script

---
+++ scripts/publish.js
@@ -0,0 +1,56 @@
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+// import chalk from 'chalk';
+const { promisify } = require('util')
+const { exec } = require('child_process');
+const execAsync = promisify(exec);
+
+(async () => {
+
+
+  /** 检查工作区是否clean */
+  const { stdout } = await execAsync(`git status -s`);
+  if (stdout) {
+    console.log(chalk.red('ERR! Git working directory not clean.'));
+    return;
+  }
+
+  const answers = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'newVersion',
+      message: 'run npm version',
+      choices: ['patch', 'minor', 'major', 'prerelease', 'prerelease:alpha', 'prerelease:beta', 'prerelease:rc', 'prerelease:stable'],
+    },
+    {
+      type: 'confirm',
+      name: 'release',
+      message: 'is release version?',
+    },
+    {
+      type: 'input',
+      name: 'npmTag',
+      message: 'npm tag(default: latest)',
+      default: 'latest'
+    }
+  ])
+
+  let { newVersion, release, npmTag } = answers;
+
+  /** npm version */
+  if (newVersion.includes('prerelease:')) {
+    const [, preid] = newVersion.split('prerelease:');
+    newVersion = `prerelease --preid=${preid}`
+  }
+
+  try {
+    await execAsync(`npm version ${newVersion}`);
+  } catch (error) {
+    console.log(chalk.red(error.stderr));
+  }
+  /** github release */
+  /** npm tag */
+
+
+
+})()
